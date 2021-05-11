9ac5489f5c3919d33534d0ff95c1029ad9483941
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-05-11 09:55:27 +0200

    Responsive and mobile animations

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // CSS
 import './style.scss';
 
 function NavBar() {
+  // un state qui permettra de gérer l'affichage du menu burger en vue mobile
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  }
+
     return (
-      <nav className="navbar">
-        <ul className="navbar__itemList">
-            <li className="navbar__itemList__item">Home</li>
-            <li className="navbar__itemList__item">About</li>
-            <li className="navbar__itemList__item">Contact</li>
-            <li className="navbar__itemList__item">Blog</li>
-            <li className="navbar__itemList__item">Careers</li>
-        </ul>
-      </nav>
+      <>
+      <div className={`${showLinks ? "modal" : "hideModal"}`}>
+      </div>
+        <nav className={`navbar ${showLinks ? "showNavBar" : "hideNavBar"}`}>
+        {/* la classe hideNavBar n'existe pas, il s'agit simplement d'éviter qu'apparaisse
+        un false en nom de classe */}
+          <ul className="navbar__itemList">
+              <li className="navbar__itemList__item slideInDown-1">Home</li>
+              <li className="navbar__itemList__item slideInDown-2">About</li>
+              <li className="navbar__itemList__item slideInDown-3">Contact</li>
+              <li className="navbar__itemList__item slideInDown-4">Blog</li>
+              <li className="navbar__itemList__item slideInDown-5">Careers</li>
+          </ul>
+          <button className="navbar__burger" onClick={handleShowLinks}>
+            <span className="navbar__burger__bar"></span>
+          </button>
+        </nav>
+      </>  
     );
   }
   
